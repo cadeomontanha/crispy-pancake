b322f435cc2aaf6030a73457900e2e466efdb04f
Paulo Duarte <phsduarte@gmail.com>
2020-08-21 21:05:39 -0300

    Alterado README.md para os testes

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -11,13 +11,13 @@ variable "stack" {
 
 variable "vpc_cidr" {
     description = "CIDR of the VPC"
-    default = "10.0.0.0/16"
+    default = "10.1.0.0/16"
 }
 
 variable "subnet_cidr" {
     description = "Subnet CIDR blocks"
     type = "list"
-    default = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24", "10.0.4.0/24"]
+    default = ["10.1.1.0/24", "10.1.2.0/24", "10.1.3.0/24", "10.1.4.0/24"]
 }
 
 variable "instances" {
